2caf2906ba77bbe8ac30e96e2c9663ca4b4a6938
rezazeraatkar1992 <154321801+rezazeraatkar1992@users.noreply.github.com>
2024-01-10 03:24:45 +0330

    add - features page initial setup

--- src/pages/feature/page.tsx
+++ src/pages/feature/page.tsx
@@ -1,24 +1,71 @@
-import Box from '@mui/material/Box';
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Chip from '@mui/material/Chip';
+import Paper from '@mui/material/Paper';
 import TextField from '@mui/material/TextField';
-import Typography from '@mui/material/Typography';
+
+interface ChipData {
+  key: number;
+  label: string;
+}
+
+const ListItem = styled('li')(({ theme }) => ({
+  margin: theme.spacing(0.5),
+}));
 
 export default function Feature() {
+  const [chipData, setChipData] = React.useState<readonly ChipData[]>([
+    { key: 0, label: 'Angular' },
+    { key: 1, label: 'jQuery' },
+    { key: 2, label: 'Polymer' },
+    { key: 4, label: 'Vue.js' },
+  ]);
+  const [inputValue, setInputValue] = React.useState('');
+
+  const handleDelete = (chipToDelete: ChipData) => () => {
+    setChipData((chips) =>
+      chips.filter((chip) => chip.key !== chipToDelete.key)
+    );
+  };
+
+  const handleKeyPress = (event: React.KeyboardEvent) => {
+    if (event.key === ' ' || event.key === 'Enter') {
+      // Add the inputValue as a new chip
+      setChipData([
+        ...chipData,
+        { key: chipData.length, label: inputValue.trim() },
+      ]);
+      // Clear the text field
+      setInputValue('');
+    }
+  };
+
   return (
-    <Box component='form' className='p-2'>
-      <div className='flex gap-4 items-center'>
-        <Typography variant='h6' gutterBottom>
-          Features
-        </Typography>
-      </div>
-      <div className='flex gap-2 w-full'>
+    <Paper className='flex flex-col gap-4 py-6 px-4'>
+      <TextField label='Feature Name' />
+      <div>
         <TextField
-          label='Feature Name'
-          id='outlined-size-small'
-          defaultValue=''
-          size='small'
-          // className='w-2/3'
+          value={inputValue}
+          onChange={(event) => setInputValue(event.target.value)}
+          onKeyUp={handleKeyPress}
+          label='Add values'
         />
+        {chipData.map((data) => {
+          let icon;
+
+          return (
+            <ListItem key={data.key}>
+              <Chip
+                icon={icon}
+                label={data.label}
+                onDelete={
+                  data.label === 'React' ? undefined : handleDelete(data)
+                }
+              />
+            </ListItem>
+          );
+        })}
       </div>
-    </Box>
+    </Paper>
   );
 }
